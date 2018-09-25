5c5ff7839852cb4dd3777fbc6962ecd7c7a7dcd2
aperez809 <32823645+aperez809@users.noreply.github.com>
2018-09-25 18:26:43 -0400

    Added interleave()

--- HW 3/src/Strings.java
+++ HW 3/src/Strings.java
@@ -15,11 +15,17 @@ interface ILoString {
   // the first of the list alphabetically
   ILoString insert(String word);
 
-  boolean isSorted();
+  boolean isSorted(ILoString acc);
 
   ILoString reverse();
 
-  ILoString addAtEnd(String word);
+  ILoString append(String word);
+
+  boolean isSortedHelp(String first);
+
+  ILoString getRest();
+
+  ILoString interleave(ILoString rest);
 }
 
 // to represent an empty list of Strings
@@ -39,15 +45,27 @@ class MtLoString implements ILoString {
     return new ConsLoString(name, this);
   }
 
-  public boolean isSorted() {
+  public boolean isSorted(ILoString acc) {
     return true;
+  }
+
+  public boolean isSortedHelp(String that) {
+    return false;
+  }
+
+  public ILoString getRest() {
+    return this;
+  }
+
+  public ILoString interleave(ILoString that) {
+    return this;
   }
 
   public ILoString reverse() {
     return this;
   }
 
-  public ILoString addAtEnd(String word) {
+  public ILoString append(String word) {
     return new ConsLoString(word, this);
   }
 
@@ -98,21 +116,33 @@ class ConsLoString implements ILoString {
     }
   }
 
-  public boolean isSorted() {
-    //return (this.first.isSortedHelp(other) && this.rest.isSorted(other);
-    return this.first.isSortedHelp();
+  public boolean isSorted(ILoString that) {
+    //return this.first.isSortedHelp(acc) && this.rest.isSorted(acc.getRest());
+    return that.isSortedHelp(this.first) && this.rest.isSorted(that.getRest());
   }
 
-  public boolean isSortedHelp(ILoString other) {
-    return true;
+  public boolean isSortedHelp(String that) {
+    return this.first.equals(that);
+  }
+
+  public ILoString getRest() {
+    return this.rest;
+  }
+
+  public ILoString interleave(ILoString that) {
+    return new ConsLoString(this.first, that.interleave(this.rest));
+  }
+
+  public ILoString merge(ILoString that) {
+    return new ConsLoString(this.first, this.rest.append(that));
   }
 
   public ILoString reverse() {
-    return this.rest.reverse().addAtEnd(this.first);
+    return this.rest.reverse().append(this.first);
   }
 
-  public ILoString addAtEnd(String s) {
-    return new ConsLoString(this.first, this.rest.addAtEnd(s));
+  public ILoString append(String first) {
+    return new ConsLoString(this.first, this.rest.append(first));
   }
 
 
@@ -128,6 +158,8 @@ class ExamplesStrings{
                           new ConsLoString("little ",
                                   new ConsLoString("lamb.", new MtLoString())))));
 
+  ILoString marySort = mary.sort();
+
   ILoString str2 = new ConsLoString("My ", new ConsLoString("name ", new ConsLoString(
           "is ",
           new ConsLoString("Alex ",
@@ -138,6 +170,10 @@ class ExamplesStrings{
                                                   new ConsLoString("my ",
                                                           new ConsLoString("name.",
                                                                   new MtLoString()))))))))));
+
+  ILoString str2Sort = str2.sort();
+
+
 
   // test the method combine for the lists of Strings
   boolean testCombine(Tester t){
@@ -163,6 +199,27 @@ class ExamplesStrings{
                                                                     new ConsLoString("Perez ",
                                                                             new ConsLoString("Perez, ",
                                                                             new MtLoString())))))))))));
+  }
+
+  boolean testIsSorted(Tester t) {
+    return t.checkExpect(mary.isSorted(marySort), false)
+            && t.checkExpect(mary.sort().isSorted(marySort), true)
+            && t.checkExpect(str2.isSorted(str2Sort), false)
+            && t.checkExpect(str2.sort().isSorted(str2Sort), true);
+  }
+
+  boolean testInterleave(Tester t) {
+    return t.checkExpect(marySort.interleave(marySort), new ConsLoString("a ",
+            new ConsLoString("a ",
+                    new ConsLoString("had ",
+                            new ConsLoString("had ",
+                                    new ConsLoString("lamb.",
+                                            new ConsLoString("lamb.",
+                                                    new ConsLoString("little ",
+                                                            new ConsLoString("little ",
+                                                                    new ConsLoString("Mary ",
+                                                                            new ConsLoString("Mary ",
+                                                                                    new MtLoString())))))))))));
   }
 
   boolean testReverse(Tester t) {
