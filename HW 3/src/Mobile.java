f2a84fc86566de14daad343efa3065cb480f186d
aperez809 <32823645+aperez809@users.noreply.github.com>
2018-09-26 13:58:13 -0400

    boop

--- HW 3/src/Mobile.java
+++ HW 3/src/Mobile.java
@@ -1,9 +1,12 @@
+import javalib.worldcanvas.WorldCanvas;
 import tester.*;                // The tester library
 import javalib.worldimages.*;   // images, like RectangleImage or OverlayImages
 import javalib.funworld.*;      // the abstract World class and the big-bang library
 import tester.TestResults.TestEquality;
 
 import java.awt.Color;          // general colors (as triples of red,green,blue values)
+
+import static javalib.worldimages.TextImage.c;
 // and predefined colors (Red, Green, Yellow, Blue, Black, White)
 
 interface IMobile {
@@ -28,7 +31,9 @@ interface IMobile {
   int curWidthRight();
 
   //combine 2 balanced mobiles together to create
-  IMobile buildMobile(IMobile that);
+  IMobile buildMobile(IMobile that, int length, int strut);
+
+  WorldImage drawMobile();
 }
 
 class Simple implements IMobile {
@@ -70,8 +75,12 @@ class Simple implements IMobile {
     return this.curWidth();
   }
 
-  public IMobile buildMobile(IMobile that) {
-    return null;
+  public IMobile buildMobile(IMobile that, int length, int strut) {
+    return this;
+  }
+
+  public WorldImage drawMobile() {
+    return new RectangleImage(this.weight/10 * 10, this.weight/10 * 10, "solid", this.color);
   }
 
 
@@ -116,6 +125,37 @@ class Complex implements IMobile {
     return this.rightside + this.right.curWidthRight();
   }
 
+  //build a new mobile such that the lengths of the left and right
+  //struts create 0 net torque
+  public IMobile buildMobile(IMobile that, int length, int strut) {
+    return new Complex(length, 0, 0, this, that);
+  }
+
+  /*public WorldImage drawMobile() {
+    double middle = (double)(length / 2);
+    WorldImage stem = new LineImage(new Posn(0, this.length),
+            Color.BLACK).movePinhole(0, 0 - middle);
+
+    return new OverlayImage(this.left.drawMobile(), this.right.drawMobile());
+  }
+*/
+  public WorldImage drawMobile() {
+    double leftMiddle = (double)(this.leftside / 2);
+    double rightMiddle = (double)(this.rightside / 2);
+    WorldImage leftStem = new LineImage(new Posn(0, this.leftside),
+            Color.BLACK).movePinhole(0, 0 - leftMiddle);
+    WorldImage rightStem = new LineImage(new Posn(0, this.rightside),
+            Color.BLACK).movePinhole(0, 0 - rightMiddle);
+
+    return new OverlayImage(
+            new RotateImage(new OverlayImage(new RotateImage(this.left.drawMobile(), 270),
+                    leftStem).movePinhole(this.leftside, this.length),
+                    90),
+            new RotateImage(new OverlayImage(new RotateImage(this.right.drawMobile(), 270),
+                    rightStem).movePinhole(this.rightside, this.length),
+                    90));
+  }
+
   public IMobile buildMobile(IMobile that) {
     return null;
   }
@@ -158,5 +198,18 @@ class ExamplesMobiles {
   }
 
   //boolean test
+
+  boolean testFailure(Tester t) {
+    return t.checkExpect(
+            new ScaleImageXY(new RectangleImage(60, 40, OutlineMode.SOLID, Color.GRAY), 0.5, 0.25),
+            new RectangleImage(30, 15, OutlineMode.SOLID, Color.GRAY));
+  }
+
+  boolean testDrawMobile(Tester t) {
+    WorldCanvas c = new WorldCanvas(500, 500);
+    WorldScene s = new WorldScene(500, 500);
+    return c.drawScene(s.placeImageXY(exampleComplex.drawMobile(), 250, 250))
+            && c.show();
+  }
 }
 
