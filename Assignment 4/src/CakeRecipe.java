66b3608b3c8593000251b2098652d19937ff666d
aperez809 <32823645+aperez809@users.noreply.github.com>
2018-10-01 09:29:15 -0400

    second constructor in p2

---
+++ Assignment 4/src/CakeRecipe.java
@@ -0,0 +1,73 @@
+class CakeRecipe {
+  double flour;
+  double sugar;
+  double eggs;
+  double butter;
+  double milk;
+
+  CakeRecipe(double flour, double sugar, double eggs, double butter, double milk) {
+    if (flour == sugar) {
+      this.flour = flour;
+      this.sugar = sugar;
+    }
+    else {
+      invalidArgs(this.flour, this.sugar);
+
+    }
+
+    if (eggs == butter) {
+      this.eggs = eggs;
+      this.butter = butter;
+    }
+    else {
+      invalidArgs(this.eggs, this.butter);
+    }
+
+    if (eggs + milk == sugar) {
+      this.milk = milk;
+    }
+    else {
+      invalidArgs(this.eggs, this.milk, this.sugar);
+    }
+  }
+
+  CakeRecipe(double flour, double eggs, double milk) {
+    this.flour = flour;
+    this.sugar = flour;
+    this.eggs = eggs;
+    this.butter = eggs;
+
+    if (eggs + milk == sugar) {
+      this.milk = milk;
+    }
+    else {
+      invalidArgs(this.eggs, this.milk, this.sugar);
+    }
+  }
+
+  Exception invalidArgs(double arg1, double arg2) {
+    throw new IllegalArgumentException("Unequal "
+            + arg1
+            + " + "
+            + arg2
+            + " arguments: "
+            + Double.toString(arg1)
+            + ", "
+            + Double.toString(arg2));
+  }
+
+  Exception invalidArgs(double arg1, double arg2, double arg3) {
+    throw new IllegalArgumentException("Weight of "
+            + arg1
+            + " + "
+            + arg2
+            + " not equal to "
+            + arg3
+            + ": "
+            + Double.toString(arg1)
+            + " + "
+            + Double.toString(arg2)
+            + " != "
+            + Double.toString(arg3));
+
+}
